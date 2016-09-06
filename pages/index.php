7931508cebf9dc326a61f8ae1c27c01ad28619cb
shubhamkhan <shubhamkhanit@gmail.com>
2016-09-06 19:29:01 +0530

    First structure submission

---
+++ pages/index.php
@@ -0,0 +1,93 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="description" content="">
+  <meta name="author" content="">
+
+  <title>SaptakMusicalHut</title>
+
+  <!-- Bootstrap Core CSS -->
+  <link href="../css/bootstrap.min.css" rel="stylesheet">
+  <!-- MetisMenu CSS -->
+  <link href="../css/metisMenu.min.css" rel="stylesheet">
+  <!-- Timeline CSS -->
+  <link href="../css/timeline.css" rel="stylesheet">
+  <!-- Bootstrap UI CSS -->
+  <link href="../css/bootstrap-theme.min.css" rel="stylesheet">
+  <!-- Jquery UI CSS -->
+  <link href="../css/jquery-ui.min.css" rel="stylesheet">
+  <!-- DataTables CSS -->
+  <link href="../css/dataTables.bootstrap.css" rel="stylesheet">
+  <!-- DataTables Responsive CSS -->
+  <link href="../css/dataTables.responsive.css" rel="stylesheet">
+  <!-- Morris Charts CSS -->
+  <link href="../css/morris.css" rel="stylesheet">
+  <!-- Custom CSS -->
+  <link href="../css/sb-admin-2.css" rel="stylesheet">
+  <!-- Custom Fonts -->
+  <link href="../css/font-awesome.min.css" rel="stylesheet">
+  <!-- Custom CSS -->
+  <link href="../css/style.css" rel="stylesheet">
+  <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+  <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+  <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+    <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+  <![endif]-->
+
+</head>
+<body>
+  <header>
+      <h1></h1>
+  </header>
+  <nav>
+  </nav>
+  <article>
+  </article>
+  <aside>
+  </aside>
+  <section>
+  </section>
+  <footer>
+    <hr />
+    <div>
+      Copyright &copy; 2016. All rights reserved.
+    </div>
+
+    <div>
+      <!-- jQuery Core JavaScript -->
+      <script src="../js/jquery.min.js"></script>
+      <!--jquery UI JavaScript-->
+      <script src="../js/jquery-ui.min.js"></script>
+      <!-- Bootstrap Core JavaScript -->
+      <script src="../js/bootstrap.min.js"></script>
+      <!-- Metis Menu Plugin JavaScript -->
+      <script src="../js/metisMenu.min.js"></script>
+      <!-- DataTables JavaScript -->
+      <script src="../js/dataTables.bootstrap.js"></script>
+      <!-- DataTables Responsive JavaScript-->
+      <script src="../js/dataTables.responsive.js"></script>
+      <!-- Morris Charts JavaScript -->
+      <script src="../js/morris.js"></script>
+      <!-- Morris Charts Data JavaScript -->
+      <script src="../js/morris-data.js"></script>
+      <!-- Morris image JavaScript -->
+      <script src="../js/raphael.js"></script>
+      <!-- Custom Theme JavaScript -->
+      <script src="../js/sb-admin-2.js"></script>
+      <!-- Custom JavaScript -->
+      <script src="../js/script.js"></script>
+      <script>
+      $(document).ready(function() {
+         $('#dataTables-example').DataTable({
+                 responsive: true
+         });
+       });
+      </script>
+    </div>
+  </footer>
+</body>
+</html>
