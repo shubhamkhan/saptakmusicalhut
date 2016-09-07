b55063a9d186f0fb7a2cf3e2b299645c0897e16f
shubhamkhan <shubhamkhanit@gmail.com>
2016-09-07 12:44:55 +0530

    Menu Bar Design

--- pages/index.php
+++ pages/index.php
@@ -43,51 +43,160 @@
   <header>
       <h1></h1>
   </header>
-  <nav>
+  <nav class="navbar navbar-default navbar-fixed-top" role="navigation">
+    <div class="container-fluid">
+      <!-- Brand and toggle get grouped for better mobile display -->
+      <div class="navbar-header">
+        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+          <span class="sr-only">Toggle navigation</span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </button>
+        <a class="navbar-brand" href="#">Saptak Musical Hut</a>
+      </div>
+      <!-- Collect the nav links, forms, and other content for toggling -->
+      <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+        <form class="navbar-form navbar-right">
+          <div class="input-group">
+            <input type="text" class="form-control" placeholder="Search for...">
+            <span class="input-group-btn">
+              <button class="glyphicon glyphicon-search btn btn-default" type="button"></button>
+            </span>
+          </div>
+        </form>
+        <ul class="nav navbar-nav">
+          <li><a href="#">HOME<span class="sr-only">(current)</span></a></li>
+          <li><a href="#">MUSICAL INSTRUMENTS</a></li>
+          <li class="dropdown">
+            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">HOT DEALS<span class="caret"></span></a>
+            <ul class="dropdown-menu">
+              <li><a href="#">Action</a></li>
+              <li><a href="#">Another action</a></li>
+              <li><a href="#">Something else here</a></li>
+              <li role="separator" class="divider"></li>
+              <li><a href="#">Separated link</a></li>
+              <li role="separator" class="divider"></li>
+              <li><a href="#">One more separated link</a></li>
+            </ul>
+          </li>
+          <li><a href="#">STUDIO/RECORDING</a></li>
+          <li class="dropdown dropdown-large center">
+    				<a href="#" class="dropdown-toggle" data-toggle="dropdown">PRO AV<b class="caret"></b></a>
+    				<ul class="dropdown-menu dropdown-menu-large row">
+    					<li class="col-sm-3">
+    						<ul>
+    							<li class="dropdown-header">Glyphicons</li>
+    							<li><a href="#">Available glyphs</a></li>
+    							<li><a href="#">How to use</a></li>
+    							<li><a href="#">Examples</a></li>
+    							<li class="divider"></li>
+    							<li class="dropdown-header">Dropdowns</li>
+    							<li><a href="#">Example</a></li>
+    							<li><a href="#">Aligninment options</a></li>
+    							<li><a href="#">Headers</a></li>
+    							<li><a href="#">Disabled menu items</a></li>
+    						</ul>
+    					</li>
+    					<li class="col-sm-3">
+    						<ul>
+    							<li class="dropdown-header">Button groups</li>
+    							<li><a href="#">Basic example</a></li>
+    							<li><a href="#">Button toolbar</a></li>
+    							<li><a href="#">Sizing</a></li>
+    							<li><a href="#">Nesting</a></li>
+    							<li><a href="#">Vertical variation</a></li>
+    							<li class="divider"></li>
+    							<li class="dropdown-header">Button dropdowns</li>
+    							<li><a href="#">Single button dropdowns</a></li>
+    						</ul>
+    					</li>
+    					<li class="col-sm-3">
+    						<ul>
+    							<li class="dropdown-header">Input groups</li>
+    							<li><a href="#">Basic example</a></li>
+    							<li><a href="#">Sizing</a></li>
+    							<li><a href="#">Checkboxes and radio addons</a></li>
+    							<li class="divider"></li>
+    							<li class="dropdown-header">Navs</li>
+    							<li><a href="#">Tabs</a></li>
+    							<li><a href="#">Pills</a></li>
+    							<li><a href="#">Justified</a></li>
+    						</ul>
+    					</li>
+    					<li class="col-sm-3">
+    						<ul>
+    							<li class="dropdown-header">Navbar</li>
+    							<li><a href="#">Default navbar</a></li>
+    							<li><a href="#">Buttons</a></li>
+    							<li><a href="#">Text</a></li>
+    							<li><a href="#">Non-nav links</a></li>
+    							<li><a href="#">Component alignment</a></li>
+    							<li><a href="#">Fixed to top</a></li>
+    							<li><a href="#">Fixed to bottom</a></li>
+    							<li><a href="#">Static top</a></li>
+    							<li><a href="#">Inverted navbar</a></li>
+    						</ul>
+    					</li>
+    				</ul>
+    			</li>
+          <li><a href="#">HOME AUDIO</a></li>
+          <li><a href="#">DJ GEAR</a></li>
+          <li><a href="#">ZINE</a></li>
+          <li><a href="#">ENTERTAINMENT</a></li>
+        </ul>
+      </div> <!-- /.navbar-collapse -->
+    </div><!-- /.container-fluid -->
   </nav>
   <article>
   </article>
   <aside>
   </aside>
   <section>
+    <div class="container">
+      <div class="page-header">
+        <h1>Sticky footer with fixed navbar</h1>
+      </div>
+      <p class="lead">Pin a fixed-height footer to the bottom of the viewport in desktop browsers with this custom HTML and CSS. A fixed navbar has been added with <code>padding-top: 60px;</code> on the <code>body &gt; .container</code>.</p>
+      <p>Back to <a href="../sticky-footer">the default sticky footer</a> minus the navbar.</p>
+    </div>
   </section>
-  <footer>
-    <hr />
-    <div>
-      Copyright &copy; 2016. All rights reserved.
+  <footer class="footer">
+    <hr/>
+    <div class="container">
+      <div class="center text-muted">
+        Copyright &copy; 2016. All rights reserved.
+      </div>
     </div>
-
-    <div>
-      <!-- jQuery Core JavaScript -->
-      <script src="../js/jquery.min.js"></script>
-      <!--jquery UI JavaScript-->
-      <script src="../js/jquery-ui.min.js"></script>
-      <!-- Bootstrap Core JavaScript -->
-      <script src="../js/bootstrap.min.js"></script>
-      <!-- Metis Menu Plugin JavaScript -->
-      <script src="../js/metisMenu.min.js"></script>
-      <!-- DataTables JavaScript -->
-      <script src="../js/dataTables.bootstrap.js"></script>
-      <!-- DataTables Responsive JavaScript-->
-      <script src="../js/dataTables.responsive.js"></script>
-      <!-- Morris Charts JavaScript -->
-      <script src="../js/morris.js"></script>
-      <!-- Morris Charts Data JavaScript -->
-      <script src="../js/morris-data.js"></script>
-      <!-- Morris image JavaScript -->
-      <script src="../js/raphael.js"></script>
-      <!-- Custom Theme JavaScript -->
-      <script src="../js/sb-admin-2.js"></script>
-      <!-- Custom JavaScript -->
-      <script src="../js/script.js"></script>
-      <script>
-      $(document).ready(function() {
-         $('#dataTables-example').DataTable({
-                 responsive: true
-         });
+    <!-- jQuery Core JavaScript -->
+    <script src="../js/jquery.min.js"></script>
+    <!--jquery UI JavaScript-->
+    <script src="../js/jquery-ui.min.js"></script>
+    <!-- Bootstrap Core JavaScript -->
+    <script src="../js/bootstrap.min.js"></script>
+    <!-- Metis Menu Plugin JavaScript -->
+    <script src="../js/metisMenu.min.js"></script>
+    <!-- DataTables JavaScript -->
+    <script src="../js/dataTables.bootstrap.js"></script>
+    <!-- DataTables Responsive JavaScript-->
+    <script src="../js/dataTables.responsive.js"></script>
+    <!-- Morris Charts JavaScript -->
+    <script src="../js/morris.js"></script>
+    <!-- Morris Charts Data JavaScript -->
+    <script src="../js/morris-data.js"></script>
+    <!-- Morris image JavaScript -->
+    <script src="../js/raphael.js"></script>
+    <!-- Custom Theme JavaScript -->
+    <script src="../js/sb-admin-2.js"></script>
+    <!-- Custom JavaScript -->
+    <script src="../js/script.js"></script>
+    <script>
+    $(document).ready(function() {
+       $('#dataTables-example').DataTable({
+               responsive: true
        });
-      </script>
-    </div>
+    });
+    </script>
   </footer>
 </body>
 </html>
